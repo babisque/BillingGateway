fa8718f19c4a71ee2a475c4c9146e61e5be23900
Babisque <rodrigobabisque1@gmail.com>
2025-07-14 13:00:18 -0300

    refactor: remove Password property from RegisterCustomerCommand

--- src/BillingGateway.Application/Handlers/Customer/Register/RegisterCustomerCommand.cs
+++ src/BillingGateway.Application/Handlers/Customer/Register/RegisterCustomerCommand.cs
@@ -10,6 +10,4 @@ public class RegisterCustomerCommand : IRequest<Result>
     public required string FullName { get; set; }
     [Required, EmailAddress]
     public required string Email { get; set; }
-    [Required, MinLength(6)]
-    public required string Password { get; set; }
 }
